a3c4a3231f2938cc644fadfda3cfb1b2058cd572
Andi waluyo <andynorthgoal@gmail.com>
2017-04-08 11:53:06 +0700

    Update Fibonancy.java

--- JURNAL_MOD8/Fibonancy.java
+++ JURNAL_MOD8/Fibonancy.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-public class Fibonancy{
+public class SumFibonancy{
 	public static void main(String[]args){
 		 Scanner scanner = new Scanner(System.in);
-		 int n1=1,n2=1,n3,i,count; 
+		 int n1=1,n2=1,n3,i,count,sum=0; 
 		 System.out.print("Masukkan Angka: ");
 
-		 count = scanner.nextInt();
-		 System.out.print(n1+" "+n2);  
-		  
+		 count = scanner.nextInt();  
+		 sum=sum+n1+n2;
 		 for(i=2;i<count;++i)  
 		 {  
-		  n3=n1+n2;  
-		  System.out.print(" "+n3);  
+		  n3=n1+n2;
+		  sum=sum+n3;
 		  n1=n2;  
-		  n2=n3;  
+		  n2=n3;
 		 }
+		 System.out.print(" "+sum);  
 	}
 }
