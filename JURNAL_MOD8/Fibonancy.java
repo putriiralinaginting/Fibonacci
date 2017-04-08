a24f5ba8635643baef9820392fe6e309468aa72a
Deden <dedennursyadid@gmail.com>
2017-04-08 12:13:35 +0700

    Update Fibonancy.java

--- JURNAL_MOD8/Fibonancy.java
+++ JURNAL_MOD8/Fibonancy.java
@@ -3,7 +3,7 @@ public class Fibonancy{
 	public static void main(String[]args){
 		 Scanner scanner = new Scanner(System.in);
 		 int n1=1,n2=1,n3,i,count; 
-		 System.out.print("Masukkan Angka: ");
+		 System.out.print("Masukkan Angka: ");// untuk memasukkan inputann angka
 
 		 count = scanner.nextInt();
 		 System.out.print(n1+" "+n2);  
@@ -11,7 +11,7 @@ public class Fibonancy{
 		 for(i=2;i<count;++i)  
 		 {  
 		  n3=n1+n2;  
-		  System.out.print(" "+n3);  
+		  System.out.print(" "+n3);  //hasil inputan 
 		  n1=n2;  
 		  n2=n3;  
 		 }
